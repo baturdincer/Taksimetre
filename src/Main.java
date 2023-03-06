74c13e70588e884726dae0d04c3b739b9f44d1cb
baturdincer <baturdincerr@gmail.com>
2023-03-06 18:11:47 +0300

    first commit!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double ucret;
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Kaç km yol gidildi: ");
+        double yol= scanner.nextDouble();
+        ucret = 10 + (yol*2.20);
+        boolean yirmiMi= ucret<=20;
+        double sonTutar = yirmiMi? 20 : ucret;
+        System.out.println("Taksimetre tutarı: "+sonTutar);
+
     }
 }
